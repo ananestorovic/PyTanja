516945da8da9ab85cd6c3a6a31b9141301f8cb16
Ana Nestorovic <ananestorovic28@gmail.com>
2021-11-08 18:34:24 +0100

    class for agent Jocke is done

--- sprites.py
+++ sprites.py
@@ -127,6 +127,128 @@ class Aki(Agent):
         return path
 
 
+class Jocke(Agent):
+
+    def __init__(self, row, col, file_name):
+        super().__init__(row, col, file_name)
+
+
+    def get_agent_path(self, game_map, goal):
+
+        list = []
+        possible = []
+        paths=[]
+        path = [game_map[self.row][self.col]]
+
+
+        row = self.row
+        col = self.col
+        visited = [(row, col)]
+
+        flag = True
+
+        while flag:
+            if row == goal[0] and col == goal[1]:
+                break
+
+            if row > 0:
+                example = (row - 1, col)
+                if example not in visited:
+                    possible.append(game_map[row - 1][col])
+            if col > 0:
+                example = (row, col - 1)
+                if example not in visited:
+                    possible.append(game_map[row][col - 1])
+            if row < (len(game_map) - 1):
+                example = (row + 1, col)
+                if example not in visited:
+                    possible.append(game_map[row + 1][col])
+            if col < (len(game_map[0]) - 1):
+                example = (row, col + 1)
+                if example not in visited:
+                    possible.append(game_map[row][col + 1])
+
+            if not possible:
+                paths.append(((row, col), []))
+                elem = list.pop(0)
+                row = elem.row
+                col = elem.col
+
+
+
+            else:
+                neighbors_sum={}
+                elem=row, col
+                for i in possible:
+                    sum=0
+                    cnt=0
+                    if i.row > 0 and (elem!=i.row-1,i.col):
+                        element=game_map[i.row-1][i.col]
+                        sum+=element.cost()
+                        cnt+=1
+                    if i.col > 0 and (elem!=i.row,i.col-1):
+                        element = game_map[i.row][i.col-1]
+                        sum += element.cost()
+                        cnt += 1
+                    if i.row < (len(game_map) - 1) and (elem!=i.row+1,i.col):
+                        element = game_map[i.row+1][i.col]
+                        sum += element.cost()
+                        cnt += 1
+                    if i.col < (len(game_map[0]) - 1) and (elem!=i.row,i.col+1):
+                        element = game_map[i.row][i.col +1]
+                        sum += element.cost()
+                        cnt += 1
+                    if sum!=0:
+                        sum=sum/cnt
+                    neighbors_sum[i.position()]= sum
+
+                if (2,1) in neighbors_sum:
+                    print(neighbors_sum[(2, 1)])
+                    if (1,0) in neighbors_sum:
+                        print(neighbors_sum[(1,0)])
+
+                possible.sort(
+                    key=lambda tile: (
+                        neighbors_sum[tile.position()], -4 if tile.row < row else -3 if tile.col > col else -2 if tile.row > row else -1))
+
+                paths.append(((row,col), possible))
+                for i in possible:
+                    visited.append(i.position())
+                list.extend(possible)
+                elem = list.pop(0)
+                row = elem.row
+                col = elem.col
+                possible = []
+
+            if not list:
+                flag = False
+
+        paths=paths[-1::-1]
+        print(paths)
+        elem_r = goal[0]
+        elem_c=goal[1]
+        elem=game_map[goal[0]][goal[1]]
+
+        path_helper=[]
+
+
+
+        for onepath in paths:
+            for onetile in onepath[1]:
+                if (elem_r, elem_c) == onetile.position():
+                    elem_r, elem_c = onepath[0]
+                    path_helper.append(onetile)
+
+        path_helper.extend(path)
+        path_helper=path_helper[-1::-1]
+        path=path_helper
+
+
+        return path
+
+# class Draza(Agent):
+
+
 class Tile(BaseSprite):
     def __init__(self, row, col, file_name):
         super(Tile, self).__init__(row, col, file_name)
